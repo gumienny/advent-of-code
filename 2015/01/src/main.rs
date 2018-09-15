ace5a0339eff09b35753b7f93cf17009d536c78d
Mariusz Gumienny <gumienny@users.noreply.github.com>
2018-09-15 14:15:16 +0200

    201501 part II

--- 2015/01/src/main.rs
+++ 2015/01/src/main.rs
@@ -1,20 +1,37 @@
+#![allow(non_upper_case_globals)]
 static puzzle: &'static str = include_str!("input");
 
-fn santa(s: &str) -> i32 {
-    if s.len() == 0 {
-        return 0;
-    }
+struct Santa {
+    floor: i32,
+    position: usize,
+}
 
-    s.chars()
-        .map(|ch| match ch {
-            '(' => 1,
-            ')' => -1,
-            _ => 0,
-        }).sum()
+impl Santa {
+    fn new(s: &str) -> Self {
+        let mut acc = 0i32;
+        let trace: Vec<_> = (1..)
+            .zip(s.chars().map(|ch| match ch {
+                '(' => {
+                    acc += 1;
+                    acc
+                }
+                ')' => {
+                    acc -= 1;
+                    acc
+                }
+                _ => acc,
+            })).collect();
+
+        Santa {
+            floor: trace.iter().last().unwrap().1,
+            position: trace.iter().position(|p| p.1 == -1).unwrap_or(0) + 1,
+        }
+    }
 }
 
 fn main() {
-    println!("{}", santa(puzzle));
+    println!("Part 1: {}", Santa::new(puzzle).floor);
+    println!("Part 2: {}", Santa::new(puzzle).position);
 }
 
 #[cfg(test)]
@@ -22,16 +39,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
-        assert_eq!(0, santa(""));
-        assert_eq!(0, santa("(())"));
-        assert_eq!(0, santa("()()"));
-        assert_eq!(3, santa("((("));
-        assert_eq!(3, santa("(()(()("));
-        assert_eq!(3, santa("))((((("));
-        assert_eq!(-1, santa("())"));
-        assert_eq!(-1, santa("))("));
-        assert_eq!(-3, santa(")))"));
-        assert_eq!(-3, santa(")())())"));
+    fn test_part_1() {
+        assert_eq!(0, Santa::new("(())").floor);
+        assert_eq!(0, Santa::new("()()").floor);
+        assert_eq!(3, Santa::new("(((").floor);
+        assert_eq!(3, Santa::new("(()(()(").floor);
+        assert_eq!(3, Santa::new("))(((((").floor);
+        assert_eq!(-1, Santa::new("())").floor);
+        assert_eq!(-1, Santa::new("))(").floor);
+        assert_eq!(-3, Santa::new(")))").floor);
+        assert_eq!(-3, Santa::new(")())())").floor);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(1, Santa::new(")").position);
+        assert_eq!(5, Santa::new("()())").position);
     }
 }
